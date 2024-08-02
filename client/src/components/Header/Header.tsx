3d873c8b1ca88ef79d8088fdbea8037cd87dff6e
Anubhav Sahu <anubhavsahu71@gmail.com>
2024-08-03 00:08:58 +0530

    made some ui change

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -2,13 +2,13 @@ type Props = {userName: string}
 
 const Header = (props: Props) => {
   return (
-    <nav className="bg-green-400 p-3 border-solid border-l-indigo-800">
+    <nav className="bg-green-400 p-1 border-solid border-l-indigo-800 px-96">
       <div className="container mx-auto flex justify-between items-center">
         <span className="text-lg text-white font-semibold">Expense Tracking App</span>
         <span className="text-sm text-white cursor-pointer">{props.userName}</span>
       </div>
     </nav>
-  )
-}
+  );
+};
 
 export default Header
